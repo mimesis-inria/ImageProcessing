59c24caacf8ca80f1faf14de59e02878c41c512b
Bruno Marques <bruno.josue.marques@inria.fr>
2017-08-21 18:14:07 +0200

    fixing negateVector

--- utils/NegateVector.h
+++ utils/NegateVector.h
@@ -40,11 +40,11 @@ class NegateVector : public common::ImplicitDataEngine
 
   NegateVector()
       : d_src(initData(&d_src, "input_position", "input vector to negate")),
-        d_srcRot(
-            initData(&d_srcRot, "input_rotation", "input rotation to negate")),
-        d_dst(initData(&d_dst, "output_position", "negated output vector")),
-        d_dstRot(
-            initData(&d_dstRot, "output_rotation", "negated output vector"))
+//        d_srcRot(
+//            initData(&d_srcRot, "input_rotation", "input rotation to negate")),
+        d_dst(initData(&d_dst, "output_position", "negated output vector"))/*,*/
+//        d_dstRot(
+//            initData(&d_dstRot, "output_rotation", "negated output vector"))
   {
   }
 
@@ -52,16 +52,16 @@ class NegateVector : public common::ImplicitDataEngine
   void init()
   {
     addInput(&d_src);
-    addInput(&d_srcRot);
+//    addInput(&d_srcRot);
     addOutput(&d_dst);
   }
 
   void update()
   {
     d_dst.setValue(-d_src.getValue());
-    sofa::defaulttype::Matrix3 rev;
-    d_dstRot.setValue(-d_srcRot.getValue().transposed());
-    d_dst.setValue(d_dstRot.getValue() * d_dst.getValue());
+//    sofa::defaulttype::Matrix3 rev;
+//    d_dstRot.setValue(-d_srcRot.getValue().transposed());
+//    d_dst.setValue(d_dstRot.getValue() * d_dst.getValue());
   }
 
   // INPUTS
@@ -69,7 +69,7 @@ class NegateVector : public common::ImplicitDataEngine
   sofa::Data<sofa::defaulttype::Matrix3> d_srcRot;
   // OUTPUTS
   sofa::Data<sofa::defaulttype::Vector3> d_dst;
-  sofa::Data<sofa::defaulttype::Matrix3> d_dstRot;
+//  sofa::Data<sofa::defaulttype::Matrix3> d_dstRot;
 };
 
 SOFA_DECL_CLASS(NegateVector)
