245254f20447a73c826ef83e8b246e7cbefef26b
Bruno Marques <bruno.josue.marques@inria.fr>
2017-09-15 17:10:21 +0200

    [ProcessOR] FIX: TemplateMatcher's pre-conversion of images now to be done by the user

--- imgproc/TemplateMatcher.h
+++ imgproc/TemplateMatcher.h
@@ -27,6 +27,7 @@
 
 #include <sofa/helper/OptionsGroup.h>
 
+#include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
 namespace sofaor
@@ -38,60 +39,54 @@ namespace imgproc
 class TemplateMatcher : public ImageFilter
 {
  public:
-	SOFA_CLASS(TemplateMatcher, ImageFilter);
+  SOFA_CLASS(TemplateMatcher, ImageFilter);
 
-	sofa::Data<common::cvMat> d_template;
-	sofa::Data<sofa::helper::OptionsGroup> d_method;
+  sofa::Data<common::cvMat> d_template;
+  sofa::Data<sofa::helper::OptionsGroup> d_method;
 
-	TemplateMatcher()
-			: d_template(initData(&d_template, "template_img",
-														"template image to search for in the input img.")),
-				d_method(initData(&d_method, "method",
-													"comparison method to use for matching."))
-	{
-		sofa::helper::OptionsGroup* t = d_method.beginEdit();
-		t->setNames(6, "SQDIFF", "SQDIFF_NORMED", "CCORR", "CCORR_NORMED", "CCOEFF",
-								"CCOEFF_NORMED");
-		t->setSelectedItem("SQDIFF");
-		d_method.endEdit();
-	}
+  TemplateMatcher()
+      : d_template(initData(&d_template, "template_img",
+                            "template image to search for in the input img.")),
+        d_method(initData(&d_method, "method",
+                          "comparison method to use for matching."))
+  {
+    sofa::helper::OptionsGroup* t = d_method.beginEdit();
+    t->setNames(6, "SQDIFF", "SQDIFF_NORMED", "CCORR", "CCORR_NORMED", "CCOEFF",
+                "CCOEFF_NORMED");
+    t->setSelectedItem("SQDIFF");
+    d_method.endEdit();
+  }
 
-	void init()
-	{
-		registerData(&d_method);
-		ImageFilter::init();
-	}
+  void init()
+  {
+    addInput(&d_template);
+    registerData(&d_method);
+    ImageFilter::init();
+  }
 
-	void applyFilter(const cv::Mat& in, cv::Mat& out, bool)
-	{
-		if (in.empty()) return;
-		cv::Mat img_gray;
-		if (in.type() == CV_8UC4)
-			cv::cvtColor(in, img_gray, CV_BGRA2GRAY);
-		else if (in.type() == CV_8UC3)
-			cv::cvtColor(in, img_gray, CV_BGR2GRAY);
-		else
-			img_gray = in.clone();
+  void applyFilter(const cv::Mat& in, cv::Mat& out, bool)
+  {
+    if (in.empty()) return;
 
-		cv::Mat templ_gray;
-		if (d_template.getValue().type() == CV_8UC4)
-			cv::cvtColor(d_template.getValue(), templ_gray, CV_BGRA2GRAY);
-		else if (d_template.getValue().type() == CV_8UC3)
-			cv::cvtColor(d_template.getValue(), templ_gray, CV_BGR2GRAY);
-		else
-			templ_gray = d_template.getValue().clone();
+    if (in.type() != CV_8UC1)
+      msg_error("TemplateMatcher::applyFilter()")
+          << "TemplateMatcher::img must be grayscale";
+    if (d_template.getValue().type() != CV_8UC1)
+      msg_error("TemplateMatcher::applyFilter()")
+          << "TemplateMatcher::template_img must be grayscale";
 
-		cv::matchTemplate(img_gray, templ_gray, out,
-											d_method.getValue().getSelectedId());
-	}
+    out = in.clone();
+    cv::matchTemplate(in, d_template.getValue(), out,
+                      d_method.getValue().getSelectedId());
+  }
 };
 
 SOFA_DECL_CLASS(TemplateMatcher)
 
 int TemplateMatcherClass = sofa::core::RegisterObject(
-															 "the 6 Template matching algorithms from "
-															 "OpenCV's cv::matchTemplate() method")
-															 .add<TemplateMatcher>();
+                               "the 6 Template matching algorithms from "
+                               "OpenCV's cv::matchTemplate() method")
+                               .add<TemplateMatcher>();
 
 }  // namespace imgproc
 }  // namespace processor
